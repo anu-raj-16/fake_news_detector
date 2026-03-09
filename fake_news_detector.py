2d597bc7951da8c87e948cc3c50d122a3b0c24e2
anu-raj-16 <anaghag.1612@gmail.com>
2026-03-08 18:18:27 -0600

    full model

--- fake_news_detector.py
+++ fake_news_detector.py
@@ -7,3 +7,38 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
+df = pd.read_csv("news.csv")
+
+df.shape
+print(df.head())
+
+labels=df.label
+print(labels.head())
+
+# Split the dataset
+
+x_train, x_test, y_train, y_test = train_test_split(df['text'], labels, test_size = 0.2, random_state = 7)
+
+# Initialize the TfidfVectorizer
+
+tdidf_vectorizer = TfidfVectorizer(stop_words = "english", max_df = 0.7)
+
+# Fit and transform train set, transform test set
+
+tfidf_train = tdidf_vectorizer.fit_transform(x_train)
+tfidf_test = tdidf_vectorizer.transform(x_test)
+
+# Initialize a PassiveAggressiveClassifier
+
+pac = PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+
+# Predict on the test set and calculate accuracy
+
+y_pred = pac.predict(tfidf_test)
+score = accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+
+# Build confusion matrix
+
+print(confusion_matrix(y_test,y_pred, labels=['FAKE','REAL']))
